94d462bf70b597d056b124603f109b1db0950d66
Joowon Lim <katejlim@gmail.com>
2016-11-03 16:10:07 +0900

    first android commit

---
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.limjoowon.gmm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView m_ListView;
+    private ArrayAdapter<String> m_Adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Android에서 제공하는 string 문자열 하나를 출력 가능한 layout으로 어댑터 생성
+        m_Adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
+
+        // Xml에서 추가한 ListView 연결
+        m_ListView = (ListView) findViewById(R.id.chatroomlist);
+
+        // ListView에 어댑터 연결
+        m_ListView.setAdapter(m_Adapter);
+
+        // ListView 아이템 터치 시 이벤트 추가
+        m_ListView.setOnItemClickListener(onClickListItem);
+
+        // ListView에 아이템 추가
+        m_Adapter.add("채팅방1");
+
+    }
+
+    // 아이템 터치 이벤트
+    public AdapterView.OnItemClickListener onClickListItem = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            // 이벤트 발생 시 해당 아이템 위치의 텍스트를 출력
+            Toast.makeText(getApplicationContext(), m_Adapter.getItem(arg2), Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, chat1.class);
+            startActivity(intent);
+        }
+    };
+
+}
---
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/chat1.java
@@ -0,0 +1,15 @@
+package com.example.limjoowon.gmm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by LimJoowon on 2016. 11. 3..
+ */
+public class chat1 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat1);
+    }
+}
