fc919b88fa67dabce19777628fe287ab4ce9d040
Joowon Lim <katejlim@gmail.com>
2016-11-05 01:53:30 +0900

    add map

--- GMM/app/src/main/java/com/example/limjoowon/gmm/location.java
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/location.java
@@ -1,16 +1,57 @@
 package com.example.limjoowon.gmm;
 
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by LimJoowon on 2016. 11. 3..
  */
-public class location extends Activity {
+public class location extends FragmentActivity implements OnMapReadyCallback {
+    static final LatLng KAIST = new LatLng(36.37, 127.36);
+    private GoogleMap googleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        googleMap = map;
+
+        //(location, zoop level)으로 카메라 이동
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KAIST, 15));
+
+
+    if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+    &&ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+
+    {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        return;
+    }
+
+    googleMap.setMyLocationEnabled(true);
+}
+
 }
