d4f512a7c0fb0426bf037be01256c53125a9ca3f
han0ah <han0ah92@gmail.com>
2016-11-07 21:51:46 +0900

    채팅방 Activity 기본 UI 구조 설정

--- GMM/app/src/main/java/com/example/limjoowon/gmm/ChatActivity.java
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/ChatActivity.java
@@ -1,15 +1,26 @@
 package com.example.limjoowon.gmm;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by LimJoowon on 2016. 11. 3..
  */
-public class ChatActivity extends Activity {
+public class ChatActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat1);
+        setContentView(R.layout.activity_chat);
+        initUI();
+    }
+
+    /**
+     * 채팅방 Activity의 UI를 초기화 한다.
+     */
+    private void initUI() {
+        setTitle("채팅방1");
     }
 }
