18670373c02b89809000e15c5d5238987a5b66c7
han0ah <han0ah92@gmail.com>
2016-11-07 21:32:01 +0900

    우리 서버로 메시지전송을 담당하는 GMMMessenger 클래스 추가. ok-http  라이브러리 디펜던시 추가.

---
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/config/MsgServerConfig.java
@@ -0,0 +1,32 @@
+package com.example.limjoowon.gmm.config;
+
+import okhttp3.MediaType;
+
+/**
+ * 메시징 서버와 관련된 설정값들을 정의
+ * Created by kijong on 2016-11-07.
+ */
+
+public class MsgServerConfig {
+
+    /**
+     * 우리 메시징 서버 HOST URI
+     */
+    public static final String HOST_URI = "http://143.248.49.55:8888";
+
+    /**
+     * 미디어타입 JSON HTTP HEAER
+     */
+    public static final MediaType MEDIATYPE_JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    /**
+     * JSON 에서 메시지를 분류하는 Key값
+     */
+    public static String KEY_MSG = "";
+
+    /**
+     * JSON 에서 Sender를 분류하는 Key값
+     */
+    public static String KEY_SENDER = "";
+}
---
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/module/GMMMessenger.java
@@ -0,0 +1,67 @@
+package com.example.limjoowon.gmm.module;
+
+import com.example.limjoowon.gmm.GMMApplication;
+import com.example.limjoowon.gmm.config.MsgServerConfig;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * 실질적으로 메시지를 보내는 일을 담당하는 클래스
+ * Created by kijong on 2016-11-07.
+ */
+public class GMMMessenger {
+
+    /**
+     * 텍스트 메시지를 보낸다.
+     * @param chatRoomId TODO : 추후에 ChatRoomId 명시
+     * @param msg
+     */
+    public void sendTextMessage(String chatRoomId, String msg) {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            String url = MsgServerConfig.HOST_URI;
+            JSONObject obj = new JSONObject();
+            obj.put(MsgServerConfig.KEY_MSG, msg);
+            // TODO : 추후 google ID로 SENDER를 분류해야 하지 않을까?
+            obj.put(MsgServerConfig.KEY_SENDER, GMMApplication.getToken());
+
+            String json = obj.toString();
+            RequestBody body = RequestBody.create(MsgServerConfig.MEDIATYPE_JSON, json);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+
+            client.newCall(request).enqueue(onMessageCallback);
+        } catch(Exception e) {
+        }
+    }
+
+    /**
+     * 메시지 전송 결과에 대한 Callback
+     */
+    private Callback onMessageCallback = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            // TODO : 메시지 전송에 실패했을 때 알맞은 처리를 추후에 한다.
+            int debug;
+            debug = 1;
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            // TODO : 메시지 전송에 성공했을 때 알맞은 처리를 추후에 한다.
+            int debug;
+            debug = 1;
+        }
+    };
+}
