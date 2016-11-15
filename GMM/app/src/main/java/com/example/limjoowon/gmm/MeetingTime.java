8e8520ff60e46338ae7efc118cb8756c0675da34
Joowon Lim <katejlim@gmail.com>
2016-11-15 21:36:18 +0900

    add time popup

---
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/MeetingTime.java
@@ -0,0 +1,16 @@
+package com.example.limjoowon.gmm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LimJoowon on 2016. 11. 15..
+ */
+public class MeetingTime extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+    }
+
+}
