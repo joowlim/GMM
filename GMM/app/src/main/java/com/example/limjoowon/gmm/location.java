ea22e9ad38e1572b0a0526bb0a4617586f766685
joowon <katejlim@gmail.com>
2016-11-30 20:12:39 +0900

    지도 다시 띄우다

--- GMM/app/src/main/java/com/example/limjoowon/gmm/location.java
+++ GMM/app/src/main/java/com/example/limjoowon/gmm/location.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,7 +21,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Created by LimJoowon on 2016. 11. 3..
  */
-public class location extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener  {
+public class location extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener  {
     static final LatLng KAIST = new LatLng(36.37, 127.36);
     private GoogleMap googleMap;
     Marker myLocMarker;
@@ -36,16 +36,12 @@ public class location extends FragmentActivity implements OnMapReadyCallback, Vi
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Button set_my_location =(Button)findViewById(R.id.set_my_location);
         set_my_location.setOnClickListener(this);
-        Button dont_know =(Button)findViewById(R.id.dont_know);
-        dont_know.setOnClickListener(this);
         Button set_group_location =(Button)findViewById(R.id.set_group_location);
         set_group_location.setOnClickListener(this);
-        Button remove_location =(Button)findViewById(R.id.remove_location);
-        remove_location.setOnClickListener(this);
-        Button absent =(Button)findViewById(R.id.absent);
-        remove_location.setOnClickListener(this);
         Button recommend =(Button)findViewById(R.id.recommend);
         recommend.setOnClickListener(this);
 
@@ -84,9 +80,6 @@ public class location extends FragmentActivity implements OnMapReadyCallback, Vi
     public void onClick(View v) {
         //여기에 할 일을 적어주세요.
         switch(v.getId()){
-            case R.id.remove_location:
-                finish();
-                break;
             case R.id.set_my_location:
                 if (myLocMarker == null) {
                     myLocMarker = googleMap.addMarker(new MarkerOptions()
@@ -113,5 +106,10 @@ public class location extends FragmentActivity implements OnMapReadyCallback, Vi
                 break;
         }
 
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
